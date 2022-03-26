b186e847eced0bc52b5a6bcb2831072c3c0b2b53
ventura94 <arianventura94@gmail.cm>
2022-03-26 19:51:48 -0400

    lock thread

--- src/MT5BotsFramework/status.py
+++ src/MT5BotsFramework/status.py
@@ -1,6 +1,7 @@
 """
 Status module.
 """
+from threading import Lock
 import MetaTrader5
 
 
@@ -10,11 +11,13 @@ class StatusMeta(type):
     """
 
     _instances = {}
+    _lock: Lock = Lock()
 
     def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
+        with cls._lock:
+            if cls not in cls._instances:
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[cls] = instance
         return cls._instances[cls]
 
 
