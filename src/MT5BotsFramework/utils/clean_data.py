d0d52df03575cf553d27ac723da308780ce49e14
ventura94 <arianventura94@gmail.cm>
2022-04-20 15:36:24 -0400

    doc clean data module

--- src/MT5BotsFramework/utils/clean_data.py
+++ src/MT5BotsFramework/utils/clean_data.py
@@ -1,10 +1,16 @@
+"""
+Clean data Module
+"""
 from inspect import signature
-from typing import Type, TypeVar, Dict, Union, Any
+from typing import TypeVar, Dict, Union
 
 T = TypeVar("T")
 
 
 def clean_data_to_dict(self: object) -> Dict[str, Union[str, float, int]]:
+    """
+    Delete None types and return dictionary
+    """
     data = {}
     for field in signature(self.__class__).parameters:
         if getattr(self, field) is not None:
