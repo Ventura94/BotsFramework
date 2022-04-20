1c24076618da4dae0b8eb9295c5fefa60f46d6a7
ventura94 <arianventura94@gmail.cm>
2022-04-20 15:34:36 -0400

    delete utils not used

--- src/MT5BotsFramework/utils/clean_data.py
+++ src/MT5BotsFramework/utils/clean_data.py
@@ -10,14 +10,3 @@ def clean_data_to_dict(self: object) -> Dict[str, Union[str, float, int]]:
         if getattr(self, field) is not None:
             data[field] = getattr(self, field)
     return data
-
-
-def parse_data_model(cls: Type[T], **kwargs: Any) -> T:
-    cls_fields = {field for field in signature(cls).parameters}
-    native_args, new_args = {}, {}
-    for name, val in kwargs.items():
-        if name in cls_fields:
-            native_args[name] = val
-        else:
-            raise ValueError(f"{name} is not a valid field for {cls.__name__}")
-    return cls(**native_args)
